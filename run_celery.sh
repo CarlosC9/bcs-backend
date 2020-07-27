ebe8d00fe8f2f3702c69dbc95a33780296edf4a5
Rafael Nebot Medina <rnebot@gmail.com>
2020-07-27 13:01:33 +0100

    Tasks module "main" to "definitions"; script develop Celery env prepare

--- run_celery.sh
+++ run_celery.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Prepare Celery for development of "bcs-backend"
-docker run -name redis --rm -d -p 6379:6379 redis
-cd ~/GoogleDrive/AA_NEXTGENDEM/bcs-backend/
+if [ ! "$(docker ps -q -f name=redis)" ]; then
+  docker run -name redis --rm -d -p 6379:6379 redis
+fi
+
+if ["$(whoami)" == "rnebot"]; then
+  cd ~/GoogleDrive/AA_NEXTGENDEM/bcs-backend/
+elif [ "$(whoami)" == "acurbelo" ]; then
+  cd .
+fi
+
 # Worker AND Beat (only for development; NOT for production -use Supervisor and two separate processes-)
-celery -A biobarcoding.tasks.main worker --beat --loglevel=info
+celery -A biobarcoding.tasks.definitions worker --beat --loglevel=info
