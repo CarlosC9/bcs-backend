8ecfe070d1731df7901440aeccd8c6d009c0830d
Rafael Nebot Medina <rnebot@gmail.com>
2020-07-27 13:04:08 +0100

    Refactoring of application entry point module

--- biobarcoding/rest/main.py
+++ biobarcoding/rest/main.py
@@ -9,32 +9,41 @@ from biobarcoding.rest.bos import bp_bos
 from biobarcoding.rest.gui_static import bp_gui
 from biobarcoding.tasks import initialize_celery
 
+# Flask and configuration file
+
 app = Flask(__name__)
 app.debug = True
 UPLOAD_FOLDER = '/tmp/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 load_configuration_file(app)
-initialize_database(app)
-d = construct_session_persistence_backend(app)
-app.config.update(d)
-
 FlaskSessionServerSide(app)  # Flask Session
 CORS(app,                    # CORS
      resources={r"/api/*": {"origins": "*"}},
      supports_credentials=True
      )
-app.logger.setLevel(log_level)
-logger.setLevel(log_level)
 
+# All BluePrints
 for bp in [bp_bos, bp_gui]:
     app.register_blueprint(bp)
 
+# Database
+initialize_database(app)
+
+# Session persistence
+d = construct_session_persistence_backend(app)
+app.config.update(d)
+
+# Celery
 initialize_celery(app)
+
+# Logger
+app.logger.setLevel(log_level)
+logger.setLevel(log_level)
 
 
 @app.route("/test")
 def test_rest_open():
-    from biobarcoding.tasks.main import add
+    from biobarcoding.tasks.definitions import add
     add.delay(2, 3)
     return "<h1 style='color:blue'>Test!</h1>"
 
