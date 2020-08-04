ae5a6bc7d6c052c4664ca912023b755743304600
Rafael Nebot Medina <rnebot@gmail.com>
2020-08-04 14:45:22 +0100

    Enable use of PostgreSQL native versioning; New required package

--- biobarcoding/db_models/__init__.py
+++ biobarcoding/db_models/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker, class_mapper, ColumnProperty, RelationshipProperty
 from sqlalchemy_continuum import make_versioned
 
-make_versioned(user_cls=None)
+make_versioned(user_cls=None, options={'native_versioning': True})
 
 DBSession = scoped_session(sessionmaker())
 
