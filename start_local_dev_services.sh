cecadfb9c8edaaa324a1a33736134f8bfd1d7f4c
Daniel Reyes <dreyes@itccanarias.org>
2020-11-03 13:29:42 +0000

    Add configuration of backend to daniel

--- start_local_dev_services.sh
+++ start_local_dev_services.sh
@@ -13,8 +13,8 @@ if [ ! "$(docker ps -q -f name=postgres_devel)" ] ; then
     docker run --name postgres_devel -d -p 5432:5432 --rm -e POSTGRES_PASSWORD=postgres -e INSTALL_CHADO_SCHEMA=1 -e INSTALL_YEAST_DATA=0 -e PGDATA=/var/lib/postgresql/data/ -v /var/lib/nextgendem/pg_devel:/var/lib/postgresql/data quay.io/galaxy-genome-annotation/chado:1.31-jenkins97-pg9.5
   elif [ "$(whoami)" == "pmoreno" ] ; then
     docker run --name postgres_devel -d -p 5432:5432 --rm -e POSTGRES_PASSWORD=postgres -v <........>:/var/lib/postgresql/data postgres
-  elif [ "$(whoami)" == "dreyes" ] ; then
-    docker run --name postgres_devel -d -p 5432:5432 --rm -e POSTGRES_PASSWORD=postgres -v <........>:/var/lib/postgresql/data postgres
+  elif [ "$(whoami)" == "daniel" ] ; then
+    docker run --name postgres_devel -d -p 5432:5432 --rm -e POSTGRES_PASSWORD=postgres -e INSTALL_CHADO_SCHEMA=1 -e INSTALL_YEAST_DATA=0 -e PGDATA=/var/lib/postgresql/data/ -v /home/daniel/Documentos/DATOS/pg_devel:/var/lib/postgresql/data quay.io/galaxy-genome-annotation/chado:1.31-jenkins97-pg9.5
   fi
 fi
 
@@ -26,8 +26,8 @@ elif [ "$(whoami)" == "acurbelo" ] ; then
   ./venv/bin/activate
 elif [ "$(whoami)" == "pmoreno" ] ; then
   cd ~/Proyectos/NEXTGENDEM/bcs-backend/
-elif [ "$(whoami)" == "dreyes" ] ; then
-  cd ~/Proyectos/NEXTGENDEM/bcs-backend/
+elif [ "$(whoami)" == "daniel" ] ; then
+  cd /home/daniel/Documentos/GIT/bcs-backend/
 fi
 
 # CELERY
