a2203e1f11b0a9518e8d3324d8b74d4802c8e48f
Rafael Nebot Medina <rnebot@gmail.com>
2020-11-11 09:58:05 +0000

    Changing bioinformatic objects key from Int to BigInt
    Adding

--- biobarcoding/db_models/bioinformatics.py
+++ biobarcoding/db_models/bioinformatics.py
@@ -1,5 +1,5 @@
 # BIOINFORMATIC data
-from sqlalchemy import Column, Integer, ForeignKey, UnicodeText, Unicode, String
+from sqlalchemy import Column, Integer, ForeignKey, UnicodeText, Unicode, String, BigInteger
 
 from biobarcoding.db_models import ORMBase, GUID, ObjectType
 
@@ -9,7 +9,7 @@ prefix = "bo_"
 class BioinformaticObject(ORMBase):
     __versioned__ = {}
     __tablename__ = f"{prefix}bos"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
     uuid = Column(GUID, unique=True)
     bo_type_id = Column(Integer, ForeignKey(ObjectType.id))
     name = Column(String(80))
@@ -36,7 +36,7 @@ class Sequence(BioinformaticObject):
         'polymorphic_identity': 'sequence',
     }
 
-    id = Column(Integer, ForeignKey(BioinformaticObject.id), primary_key=True)
+    id = Column(BigInteger, ForeignKey(BioinformaticObject.id), primary_key=True)
 
 
 class MultipleSequenceAlignment(BioinformaticObject):
@@ -46,7 +46,7 @@ class MultipleSequenceAlignment(BioinformaticObject):
         'polymorphic_identity': 'msa',
     }
 
-    id = Column(Integer, ForeignKey(BioinformaticObject.id), primary_key=True)
+    id = Column(BigInteger, ForeignKey(BioinformaticObject.id), primary_key=True)
 
 
 class PhylogeneticTree(BioinformaticObject):
@@ -56,4 +56,4 @@ class PhylogeneticTree(BioinformaticObject):
         'polymorphic_identity': 'phylo-tree',
     }
 
-    id = Column(Integer, ForeignKey(BioinformaticObject.id), primary_key=True)
+    id = Column(BigInteger, ForeignKey(BioinformaticObject.id), primary_key=True)
