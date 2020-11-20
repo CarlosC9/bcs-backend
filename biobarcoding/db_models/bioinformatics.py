94092297c8a07f6d1f153c6794ca7385a3c78b3d
Rafael Nebot Medina <rnebot@gmail.com>
2020-11-20 08:25:49 +0000

    Default value for GUID fields. New ProcessInJobManagementType

--- biobarcoding/db_models/bioinformatics.py
+++ biobarcoding/db_models/bioinformatics.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, Integer, ForeignKey, UnicodeText, Unicode, String, BigInteger
 
 from biobarcoding.db_models import ORMBase, GUID, ObjectType
+import uuid
 
 prefix = "bo_"
 
@@ -10,7 +11,7 @@ class BioinformaticObject(ORMBase):
     __versioned__ = {}
     __tablename__ = f"{prefix}bos"
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    uuid = Column(GUID, unique=True)
+    uuid = Column(GUID, unique=True, default=uuid.uuid4)
     bo_type_id = Column(Integer, ForeignKey(ObjectType.id))
     name = Column(String(80))
     content = Column(UnicodeText)
@@ -24,7 +25,7 @@ class BioinformaticObject(ORMBase):
 class BarCodingRegions(ORMBase):
     __tablename__ = f"{prefix}bar_coding_regions"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    uuid = Column(GUID, unique=True)
+    uuid = Column(GUID, unique=True, default=uuid.uuid4)
     name = Column(String(80))
     description = Column(UnicodeText)
 
