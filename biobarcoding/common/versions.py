b7f2c40ed0f4e13dcd03edfa128d2aa5e9f8fe98
Rafael Nebot Medina <rnebot@gmail.com>
2021-02-26 21:05:31 +0000

    Module to contain version management related functions

---
+++ biobarcoding/common/versions.py
@@ -0,0 +1,8 @@
+"""
+Functions to support accessing previous information, based on the use of SQLAlchemy Continuum
+
+?
+Active version at date DT: vs = version_class(...).sess.query(vs).join(transact).filter(Transaction.date <= DT).first()
+?
+
+"""
