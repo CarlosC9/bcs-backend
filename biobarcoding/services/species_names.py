7486b7f4a39b96eaecb5ea3991347541890a6516
Rafael Nebot Medina <rnebot@gmail.com>
2021-08-04 19:43:00 +0100

    "Canonical species names" function tested
    It has three structures, from fastest to slowest:
    * a Dict in memory
    * an Entity in the database
    * GBIF "GET /species/match" service

--- biobarcoding/services/species_names.py
+++ biobarcoding/services/species_names.py
@@ -2,7 +2,6 @@ import urllib.parse
 from typing import List
 import requests
 
-from biobarcoding.db_models.metadata import SpeciesNameToCanonical
 
 species_names_map = {}
 
@@ -14,13 +13,18 @@ def get_canonical_species_names(sess, in_: List[str]) -> List[str]:
     :param in_: List of species names to canonicalize
     :return: List of canonicalized species names. None if it was not possible to do
     """
-    from biobarcoding import engine, postgis_engine, get_global_configuration_variable
-    from biobarcoding.common.pg_helpers import create_pg_database_engine
-    if engine is None:
-        from biobarcoding.db_models import DBSession
-        db_connection_string = get_global_configuration_variable('DB_CONNECTION_STRING')
-        engine = create_pg_database_engine(db_connection_string, "bcs", recreate_db=False)
-        DBSession.configure(bind=engine)  # reconfigure the sessionmaker used by this scoped_session
+    from biobarcoding import engine
+    commit_after = False
+    if sess is None:
+        commit_after = True
+        if engine is None:
+            from biobarcoding.common.pg_helpers import create_pg_database_engine
+            from biobarcoding.db_models.metadata import SpeciesNameToCanonical
+            from biobarcoding.db_models import DBSession
+            from biobarcoding import get_global_configuration_variable
+            db_connection_string = get_global_configuration_variable('DB_CONNECTION_STRING')
+            engine = create_pg_database_engine(db_connection_string, "bcs", recreate_db=False)
+            DBSession.configure(bind=engine)  # reconfigure the sessionmaker used by this scoped_session
         sess = DBSession()
 
     _ = []
@@ -40,20 +44,22 @@ def get_canonical_species_names(sess, in_: List[str]) -> List[str]:
                     species_names_map[lsn] = species_name.canonical_name
             else:
                 # Match using GBIF
-                url = f"https://api.gbif.org/v1/species/match?name={urllib.parse.urlencode(sn)}"
+                url = f"https://api.gbif.org/v1/species/match?name={urllib.parse.quote_plus(sn)}"
                 response = requests.request("GET", url)
                 if response.status_code == 200:
                     species_name = SpeciesNameToCanonical()
                     species_name.name = lsn
                     r = response.json()
-                    if r["matchType"] is "NONE" or (r["matchType"] is "FUZZY" and r["confidence"] <= 70):
+                    if r["matchType"] == "NONE" or (r["matchType"] == "FUZZY" and r["confidence"] <= 70):
                         species_name.canonical_name = ""
                         species_name.scientific_name = ""
-                    elif r["matchType"] is "EXACT" or (r["matchType"] is "FUZZY" and r["confidence"] > 70):
+                    elif r["matchType"] == "EXACT" or (r["matchType"] == "FUZZY" and r["confidence"] > 70):
                         species_name.rank = r["rank"]
                         species_name.synonym = r["synonym"]
                         species_name.canonical_name = r["canonicalName"]
                         species_name.scientific_name = r["scientificName"]
+                        species_names_map[lsn] = species_name.canonical_name
+                        found = True
                     sess.add(species_name)
         else:
             found = True
@@ -62,13 +68,18 @@ def get_canonical_species_names(sess, in_: List[str]) -> List[str]:
         else:
             v = None
         _.append(v)
-    return v
+    if commit_after:
+        sess.commit()
+
+    return _
 
 
-lst = ["Euphorbia lathyris L.",
-       'Lobularia libyca (Viv.) Meisn.',
-       'Schizogyne sericea (L. f.) DC.',
-       'Wahlenbergia lobelioides (L. f.) Link subsp. lobelioides',
-       'Cistus monspeliensis L. subsp. canariensis Rivas-Mart., Martín-Osorio & Wildpret']
-s = get_canonical_species_names(None, )
-print(s)
+# lst = ["Euphorbia lathyris L.",
+#        'Lobularia libyca (Viv.) Meisn.',
+#        'Schizogyne sericea (L. f.) DC.',
+#        'Wahlenbergia lobelioides (L. f.) Link subsp. lobelioides',
+#        'Cistus monspeliensis L. subsp. canariensis Rivas-Mart., Martín-Osorio & Wildpret']
+# s = get_canonical_species_names(None, lst)
+# print(s)
+# s = get_canonical_species_names(None, lst)
+# print(s)
