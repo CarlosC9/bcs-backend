3204832003e679bde671dd99060b7a51031fa930
Alejandro <alecurfon@gmail.com>
2021-10-13 11:09:18 +0100

    hotfix, collection names

--- biobarcoding/db_models/sysadmin.py
+++ biobarcoding/db_models/sysadmin.py
@@ -172,6 +172,7 @@ class Collection(ORMBase):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     uuid = Column(GUID, nullable=False, default=uuid.uuid4)  # Object ID (Collection are on objects with UUID)
+    name = Column(String(80))
 
 
 class CollectionDetail(ORMBase):
