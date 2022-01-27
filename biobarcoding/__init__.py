9152975045dd5dfd2e4b58f192a251ab4bcd4b58
Rafael Nebot Medina <rnebot@gmail.com>
2022-01-27 13:49:11 +0000

    Fix - read configuration file from both Backend AND Celery
    From Celery it did not work correctly

--- biobarcoding/__init__.py
+++ biobarcoding/__init__.py
@@ -25,22 +25,48 @@ def expand_paths(key: str, value: str) -> str:
 
 
 def get_global_configuration_variable(key: str, default=None) -> str:
+
+    def define_config_file_var():
+        from biobarcoding.rest import complete_configuration_file, prepare_default_configuration
+        _, file_name = prepare_default_configuration(False)
+        if not os.environ.get(config_file_var):
+            if os.path.isfile(file_name):
+                found = True
+                complete_configuration_file(file_name)
+            else:
+                found = False
+            if not found:
+                cfg, file_name = prepare_default_configuration(True)
+                with open(file_name, "wt") as f:
+                    f.write(cfg)
+        else:
+            file_name = os.environ.get(config_file_var)
+            complete_configuration_file(file_name)
+        return file_name
+
     def read_configuration() -> Dict[str, str]:
         """
-        If environment variable "MAGIC_NIS_SERVICE_CONFIG_FILE" is defined, and the contents is the name of an existing file,
+        If environment variable "BCS_CONFIG_FILE" is defined, and the contents is the name of an existing file,
         read it as a configuration file and return the result
 
         :return:
         """
-        if os.environ.get(config_file_var):
+        if not os.environ.get(config_file_var):
+            fname = define_config_file_var()
+            os.environ[config_file_var] = fname
+        else:
             fname = os.environ[config_file_var]
+        if fname.strip() != "":
             if os.path.exists(fname):
                 with open(fname, 'r') as f:
                     config_string = '[asection]\n' + f.read()
                 config = configparser.ConfigParser()
                 config.read_string(config_string)
                 return {k: expand_paths(k, remove_quotes(v)) for k, v in config.items("asection")}
-        return {}
+            else:
+                return {}
+        else:
+            return {}
 
     global global_configuration
     if global_configuration is None:
