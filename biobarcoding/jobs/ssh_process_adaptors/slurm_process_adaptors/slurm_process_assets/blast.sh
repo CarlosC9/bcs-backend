66c163d117ff32d336e6408bbad0b3b3f7d52846
Daniel Reyes <danielreyesparrilla17@gmail.com>
2022-10-14 14:41:14 +0100

    Fix some bugs in path to databases for BLAST algorithm

--- biobarcoding/jobs/ssh_process_adaptors/slurm_process_adaptors/slurm_process_assets/blast.sh
+++ biobarcoding/jobs/ssh_process_adaptors/slurm_process_adaptors/slurm_process_assets/blast.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [[ $task == *"megablast"* ]]; then
-  srun blastn -query $query_filename -task $task -db "/home/dreyes/blast/$db" -num_threads $cpus_per_task -use_index true -out $output_filename
-else
-  srun blastn -query $query_filename -task $task -db "/mnt/datos/blast_databases/$db" -num_threads $cpus_per_task -out $output_filename
+if [[ $task == *"megablast"* ]] && [[ $db == "jardin_botanico" ]]; then
+  srun blastn -query $query_filename -task $task -db "/home/dreyes/blast/jardin_botanico" -num_threads $cpus_per_task -use_index true -out $output_filename
+elif [[ $db == "jardin_botanico" ]]; then
+  srun blastn -query $query_filename -task $task -db "/home/dreyes/blast/jardin_botanico" -num_threads $cpus_per_task -out $output_filename
+elif [[ $task == *"megablast"* ]] && [[ $db == "nt" ]]; then
+  srun blastn -query $query_filename -task $task -db "/mnt/datos/blast_databases/nt/nt" -num_threads $cpus_per_task -use_index true -out $output_filename
+elif [[ $db == "nt" ]]; then
+  srun blastn -query $query_filename -task $task -db "/mnt/datos/blast_databases/nt/nt" -num_threads $cpus_per_task -out $output_filename
 fi
