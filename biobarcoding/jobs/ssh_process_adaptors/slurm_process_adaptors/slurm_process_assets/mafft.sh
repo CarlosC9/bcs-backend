b25385cd0d90cb9455e3187525cbd73ea36fa5a6
Daniel Reyes <danielreyesparrilla17@gmail.com>
2022-10-14 14:41:37 +0100

    Optimize mafft slurm process

--- biobarcoding/jobs/ssh_process_adaptors/slurm_process_adaptors/slurm_process_assets/mafft.sh
+++ biobarcoding/jobs/ssh_process_adaptors/slurm_process_adaptors/slurm_process_assets/mafft.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export n=`expr $ntasks \* $cpus_per_task`
-export MAFFT_N_THREADS_PER_PROCESS="$cpus_per_task"
-export MAFFT_MPIRUN="mpirun -n $n --map-by ppr:$cpus_per_task:node -bind-to none --oversubscribe"
-
 if [[ $SLURM_CLUSTER_NAME == teidehpc ]]
 then
   source /etc/profile.d/profile.modules.sh
@@ -14,7 +10,7 @@ fi
 
 if [[ $algorithm = "--6merpair" ]]
 then
-  srun mafft --mpi --large $algorithm --retree $retree $parttree --maxiterate $maxiterate $input_filename > $output_filename
+  srun --nodes=$n --ntasks-per-node=$cpus_per_task mafft --mpi --large $algorithm --thread $cpus_per_task --retree $retree $parttree --maxiterate $maxiterate $input_filename > $output_filename
 else
-  srun mafft --mpi --large $algorithm --maxiterate $maxiterate $input_filename > $output_filename
+  srun --nodes=$n --ntasks-per-node=$cpus_per_task mafft --mpi --large $algorithm --thread $cpus_per_task --maxiterate $maxiterate $input_filename > $output_filename
 fi
